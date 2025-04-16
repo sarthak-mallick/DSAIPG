e4ecab97df849946698c6fd772abc0868215e91f
dwij <dwij0712@gmail.com>
2025-04-16 17:09:07 -0400

    MCTS for Tic Tac Toe

--- Java/src/main/java/com/phasmidsoftware/dsaipg/projects/mcts/tictactoe/TicTacToeNode.java
+++ Java/src/main/java/com/phasmidsoftware/dsaipg/projects/mcts/tictactoe/TicTacToeNode.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2024. Robin Hillyard
- */
-
 package com.phasmidsoftware.dsaipg.projects.mcts.tictactoe;
 
 import com.phasmidsoftware.dsaipg.projects.mcts.core.Node;
@@ -13,93 +9,79 @@ import java.util.Optional;
 
 public class TicTacToeNode implements Node<TicTacToe> {
 
-    /**
-     * @return true if this node is a leaf node (in which case no further exploration is possible).
-     */
-    public boolean isLeaf() {
-        return state().isTerminal();
+
+    public TicTacToeNode(State<TicTacToe> state, TicTacToeNode parent) {
+        this.state = state;
+        this.parent = parent;
+        this.children = new ArrayList<>();
+        initializeNodeData();  // IMPORTANT: restore initialization
     }
 
-    /**
-     * @return the State of the Game G that this Node represents.
-     */
+    public TicTacToeNode(State<TicTacToe> state) {
+        this(state, null);
+    }
+
+    public TicTacToeNode getParent() {
+        return parent;
+    }
+
+    @Override
+    public boolean isLeaf() {
+        return children.isEmpty();
+    }
+
+    @Override
     public State<TicTacToe> state() {
         return state;
     }
 
-    /**
-     * Method to determine if the player who plays to this node is the opening player (by analogy with chess).
-     * For this method, we assume that X goes first so is "white."
-     * NOTE: this assumes a two-player game.
-     *
-     * @return true if this node represents a "white" move; false for "black."
-     */
+    @Override
     public boolean white() {
         return state.player() == state.game().opener();
     }
 
-    /**
-     * @return the children of this Node.
-     */
+    @Override
     public Collection<Node<TicTacToe>> children() {
         return children;
     }
 
-    /**
-     * Method to add a child to this Node.
-     *
-     * @param state the State for the new chile.
-     */
+    @Override
     public void addChild(State<TicTacToe> state) {
-        children.add(new TicTacToeNode(state));
+        children.add(new TicTacToeNode(state, this));
     }
 
-    /**
-     * This method sets the number of wins and playouts according to the children states.
-     */
+    @Override
     public void backPropagate() {
-        playouts = 0;
-        wins = 0;
-        for (Node<TicTacToe> child : children) {
-            wins += child.wins();
-            playouts += child.playouts();
-        }
+        // handled externally in MCTS
     }
 
-    /**
-     * @return the score for this Node and its descendents a win is worth 2 points, a draw is worth 1 point.
-     */
+    @Override
     public int wins() {
         return wins;
     }
 
-    /**
-     * @return the number of playouts evaluated (including this node). A leaf node will have a playouts value of 1.
-     */
+    @Override
     public int playouts() {
         return playouts;
     }
 
-    public TicTacToeNode(State<TicTacToe> state) {
-        this.state = state;
-        children = new ArrayList<>();
-        initializeNodeData();
-    }
-
+    // Crucial initialization method
     private void initializeNodeData() {
-        if (isLeaf()) {
+        if (state.isTerminal()) {
             playouts = 1;
             Optional<Integer> winner = state.winner();
             if (winner.isPresent())
-                wins = 2; // CONSIDER check that the winner is the correct player. We shouldn't need to.
+                wins = 2;
             else
-                wins = 1; // a draw.
+                wins = 1;
         }
     }
 
     private final State<TicTacToe> state;
+    private final TicTacToeNode parent;
     private final ArrayList<Node<TicTacToe>> children;
 
-    private int wins;
-    private int playouts;
+    int wins;      // package-private
+    int playouts;  // package-private
+
 }
